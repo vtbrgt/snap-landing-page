194a4c812ac501a7622470a226de2c48376d4da9
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-05-14 22:11:05 -0300

    finishing mobile menu

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const btnMobile = document.querySelector(".btn_mobile");
-const nav = document.querySelector(".nav");
+const botaoMenu = document.querySelector(".botao_menu");
 
-function mostraMenu() {
+/* mostrar menu ao clicar */
+function mostraMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.querySelector(".nav");
   nav.classList.toggle("active");
 }
 
-btnMobile.addEventListener("click", mostraMenu);
+/* troca o ícone de abrir/fechar menu */
+function trocarIconeMenu() {
+  botaoMenu.classList.toggle("active");
+}
+
+botaoMenu.addEventListener("click", mostraMenu);
+botaoMenu.addEventListener("click", trocarIconeMenu);
+/* responsividade mobile (ux) */
+botaoMenu.addEventListener("touchstart", mostraMenu);
+botaoMenu.addEventListener("touchstart", trocarIconeMenu);
 
 /* remover o 'active' caso a tela seja redimensionada para resolução de desktop */
 window.addEventListener("resize", function () {
